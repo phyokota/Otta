8ab6df17d906d363804fb1d844fd324c43c0d9dc
Paul Yokota <glamingz@pauls-mbp.lan>
2023-03-19 12:28:26 -0700

    added CMakeLists.txt

---
+++ CMakeFiles/runGame.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/runGame.dir/main.cpp.o"
+  "CMakeFiles/runGame.dir/main.cpp.o.d"
+  "runGame"
+  "runGame.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/runGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/runGame.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for runGame.
+# This may be replaced when dependencies are built.
